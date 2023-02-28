dd4855b566739d0a9eb7a3138df9eaae0002f225
Nicolas Séau <seau.nicolas@gmail.com>
2023-02-28 16:25:44 +0100

    mise à jour Banniere acceuil

--- kasa/src/pages/home/Home.jsx
+++ kasa/src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Banner from '../../components/banner/Banner';
+import bannerHome from '../../images/banniere.png';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import './home.css';
@@ -7,6 +9,7 @@ const Home = () => {
     return (
         <div className='home'>
             <Header />
+            <Banner image={bannerHome} texte="Chez vous, partout et ailleurs" />
             <Footer />
         </div>
     );
